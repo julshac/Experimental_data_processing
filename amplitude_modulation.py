7daaa832463ca29122a4dc98c0077975dcceb1fc
Шац Юлия <JULSHAC@fuit.spbstu.ru>
2018-12-17 15:55:37 +0300

    Modify modulation functions
    Note:
    * Modify graphics output
    * Need to modify fourier function
    * Need to modify noise with modulation

--- amplitude_modulation.py
+++ amplitude_modulation.py
@@ -2,25 +2,25 @@ import numpy as np
 
 
 def signal(n=1000, amplitude=100):
-    x = np.arange(n)
+    _x = np.arange(n)
     for i in range(n):
-        x[i] = amplitude * np.cos(i / (10 * np.pi))
-    return x
+        _x[i] = amplitude * np.sin(i / (10 * np.pi))
+    return _x
 
 
 def modulation(m=0.5, amplitude=100, frequency=57, phase=1):
-    x = signal()
-    modul = np.arange(len(x))
-    max = np.abs(np.max(x))
-    for i in range(len(x)):
-        modul[i] = amplitude * (1 + m * x[i] / max) * np.cos(frequency * i * phase)
-    return modul
+    _x = signal()
+    _modul = np.arange(len(_x))
+    _max = np.abs(np.max(_x))
+    for i in range(len(_x)):
+        _modul[i] = amplitude * (1 + m * _x[i] / _max) * np.cos(frequency * i * phase)
+    return _modul
 
 
 def carrying_oscillation(m=0.5, amplitude=100, frequency=57):
-    x = signal()
-    modul = np.arange(len(x))
-    for i in range(len(x)):
-        modul[i] = np.abs(amplitude + m) * np.cos(frequency * i)
-    return modul
+    _x = signal()
+    _modul = np.arange(len(_x))
+    for i in range(len(_x)):
+        _modul[i] = np.abs(amplitude + m) * np.cos(frequency * i)
+    return _modul
 
